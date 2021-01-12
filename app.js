fbdfe39efc6105419c7ef6407d2df656b9b4c07f
Aiden-Thiele <aiden.thiele@versent.com.au>
2021-01-12 15:46:54 +1100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Full pipline testing'));
+app.get('/', (req, res) => res.send('This is a test to run a full line.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
